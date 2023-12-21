2cb660a0d662e56db17b8d4761da620d83e11b88
AlexandreSout0 <soutoinf@gmail.com>
2023-12-21 15:24:04 -0300

    callback finish ok

--- str-producer/src/main/java/com/souto/strproducer/services/StringProducerService.java
+++ str-producer/src/main/java/com/souto/strproducer/services/StringProducerService.java
@@ -1,9 +1,15 @@
 package com.souto.strproducer.services;
 
+
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import java.util.concurrent.CompletableFuture;
+import static org.apache.kafka.common.requests.FetchMetadata.log;
 
+@Log4j2
 @Service
 public class StringProducerService {
 
@@ -11,7 +17,17 @@ public class StringProducerService {
     private KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("str-topic", message);
+
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("str-topic", message).toCompletableFuture();
+        future.whenComplete((result, exception) -> {
+            if (exception == null) {
+                log.info("Send message with sucess {}", message );
+                log.info("Partiton => {} | Offset => {}",result.getRecordMetadata().partition(), result.getRecordMetadata().offset());
+            } else {
+                log.error("Error send message {}",message);
+            }
+        });
+
     }
 
 
